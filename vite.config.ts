2f67e26463cf08db86175c68f93ec0f61b431b3b
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-07-02 01:01:00 -0400

    More tooling fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,41 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { viteAwesomeSvgLoader } from 'vite-awesome-svg-loader';
+import Components from 'unplugin-vue-components/vite';
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
+
+// Different build output dirs for different deploy envs with different
+// base paths
+const outDir = process.env.ENV ? `dist-${process.env.ENV}` : 'dist';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteAwesomeSvgLoader(),
     vueDevTools(),
+    Components({ resolvers: BootstrapVueNextResolver() }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  // Needed until Bootstrap migrates off Node Sass and onto Sass modules
+  // Optimistic estimate: 5.5
+  // Pessimistic estimate: 6
+  // See thread: https://github.com/orgs/twbs/discussions/41370
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ['import', 'mixed-decls', 'color-functions', 'global-builtin'],
+      },
+    },
+  },
+  build: {
+    outDir,
+  },
+});
