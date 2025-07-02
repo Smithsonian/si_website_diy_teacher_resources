c3e785968d826287208f85ef0772cac7c3671d0e
Vanessa Phipps <vanessa.phipps@cfa.harvard.edu>
2025-07-02 00:48:44 -0400

    Add Bootstrap

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ViteSSG } from 'vite-ssg/single-page';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createBootstrap } from 'bootstrap-vue-next';
 
 library.add(faChevronUp, faChevronDown);
 
 export const createApp = ViteSSG(App, ({ app }) => {
   app.component('FontAwesomeIcon', FontAwesomeIcon);
+  app.use(createBootstrap());
 });
